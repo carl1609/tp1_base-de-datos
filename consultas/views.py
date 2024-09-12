9305ed144baa0312713a1d2f913503d928380ac8
cmario <ccmario2001169@gmail.com>
2024-09-12 19:23:19 -0300

    agregando consultas al diccionario

--- consultas/views.py
+++ consultas/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.db import connection
 # Create your views here.
-def prueba(request):
-    return ('hola')
+
+def index(request):
+    return render(request,'base.html')
+
+def evaluar_consultas(request,consulta):
+    consultas={
+        #aqui van las consultas
+        "consulta1":"select * from conejos",
+    }
+    #funcion para obtener la consulta me devuelve una lista de duplas con los resultados
+    def obtener_consulta(consulta):
+        with connection.cursor() as cursor:
+            cursor.execute(consultas[consulta])
+            resultado=cursor.fetchall()
+        return resultado
+    
+    resultado=obtener_consulta(consulta)
+
+    return render(request,'resultado.html',{'resultados':resultado})
+
+
+
+ 
