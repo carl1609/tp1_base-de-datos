f59222f003b20fbb4cf8c6c891396116b15034cf
cmario <ccmario2001169@gmail.com>
2024-09-13 20:51:31 -0300

    arreglando consulta

--- consultas/views.py
+++ consultas/views.py
@@ -30,7 +30,7 @@ def evaluar_consultas(request, consulta):
         # Consultas para obtener detalles de los objetos
         "esquemas-detalles": "SELECT n.nspname AS schema_name, r.rolname AS role_name, has_schema_privilege(r.rolname, n.nspname, 'USAGE') AS usage_privilege, has_schema_privilege(r.rolname, n.nspname, 'CREATE') AS create_privilege FROM pg_namespace n CROSS JOIN pg_roles r WHERE n.nspname = 'nombre_del_esquema'; -- Reemplaza 'nombre_del_esquema' con el esquema que deseas consultar",
         "tablas-detalles": "SELECT table_schema, table_name, ordinal_position, column_name, data_type FROM information_schema.columns WHERE table_schema = 'public';",
-        "tablas-primarykey": "SELECT tc.table_schema, tc.table_name, kcu.column_name FROM information_schema.table_cgionstraints AS tc JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name WHERE tc.constraint_type = 'PRIMARY KEY' AND tc.table_schema = 'public';",
+        "tablas-primarykey": "SELECT tc.table_schema, tc.table_name, kcu.column_name FROM information_schema.table_constraints AS tc JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name WHERE tc.constraint_type = 'PRIMARY KEY' AND tc.table_schema = 'public';",
         "tablas-foreignkeys": "SELECT tc.table_schema, tc.table_name, kcu.column_name, kcu.constraint_name FROM information_schema.table_constraints AS tc JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name WHERE tc.constraint_type = 'FOREIGN KEY' AND tc.table_schema = 'public';",
         "secuencias-detalles": "SELECT n.nspname AS schema_name, c.relname AS sequence_name, s.seqstart AS start_value, s.seqincrement AS increment_by, s.seqmax AS max_value, s.seqmin AS min_value, s.seqcache AS cache_size, CASE WHEN s.seqcycle THEN 'YES' ELSE 'NO' END AS is_cycled FROM pg_class c JOIN pg_namespace n ON n.oid = c.relnamespace JOIN pg_sequence s ON s.seqrelid = c.oid WHERE c.relkind = 'S' -- 'S' para secuencias AND n.nspname = 'nombre_del_esquema'; -- Reemplaza 'nombre_del_esquema' con el esquema que deseas consultar",
         "indices-definicion": "SELECT schemaname, tablename, indexname, indexdef FROM pg_indexes WHERE schemaname = 'public';",
@@ -42,7 +42,7 @@ def evaluar_consultas(request, consulta):
         # Consultas para monitoreo
         "monitorear-conexiones": "SELECT pid, usename, client_addr, query FROM pg_stat_activity;",
         "tamanio-bbdd": "SELECT datname AS database_name, pg_size_pretty(pg_database_size(datname)) AS size FROM pg_database;", #USARA GRAFICOS
-        "tamanio-tablas": "SELECT pg_size_pretty(pg_total_relation_size('apellidos_pais')) AS total_size",
+        "tamanio-tablas": "SELECT pg_size_pretty(pg_total_relation_size('conejos')) AS total_size",
         "tamanio-tablas-esquema": "SELECT table_name, pg_size_pretty(pg_total_relation_size(quote_ident(table_schema) || '.' || quote_ident(table_name))) AS size FROM information_schema.tables WHERE table_schema = 'public';",
         "listado-tamanios": "SELECT current_database() AS database_name, schemaname AS schema_name, tablename AS table_name, pg_size_pretty(pg_total_relation_size(schemaname || '.' || tablename)) AS total_size, pg_size_pretty(pg_relation_size(schemaname || '.' || tablename)) AS table_size, pg_size_pretty(pg_indexes_size(schemaname || '.' || tablename)) AS index_size FROM pg_tables;"
     }
--- tp1_base/settings.py
+++ tp1_base/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres', # CONTRASEÑA DE TU SERVIDOR postgres con el usuario postgres !!!!
-        'PASSWORD': '12345',
+        'PASSWORD': 'mario2001',
         'HOST': 'localhost',  # O la IP de tu servidor PostgreSQL
         'PORT': '5432',  # Puerto predeterminado de PostgreSQL
     }
