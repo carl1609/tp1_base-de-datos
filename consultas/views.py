3dc1304b971f3c79086cab481891c246d390e208
cmario <ccmario2001169@gmail.com>
2024-09-13 00:16:40 -0300

    haciendo el sistema de graficos y de resultados

--- consultas/views.py
+++ consultas/views.py
@@ -6,23 +6,26 @@ from django.db import connection
 def index(request):
     return render(request,'base.html')
 
-def evaluar_consultas(request,consulta):
+def evaluar_consultas(request,consulta,grafico):
     consultas={
         #aqui van las consultas
         "consulta1":"select * from conejos",
     }
+    
+
     #funcion para obtener la consulta me devuelve una lista de duplas con los resultados
     def obtener_consulta(consulta):
         with connection.cursor() as cursor:
             cursor.execute(consultas[consulta])
             resultado=cursor.fetchall()
             nombre_columnas=[desc[0] for desc in cursor.description]
-
         return resultado,nombre_columnas
     
-    resultado,nombre_columnas=obtener_consulta(consulta)
-
-    return render(request,'resultado.html',{'resultados':resultado,'columnas':nombre_columnas})
+    resultado,nombre_columnas=obtener_consulta(consulta) 
+    lista_resultado=[list(tupla) for tupla in resultado]
+    hay_grafico='no' if grafico==0 else 'si' 
+    print(lista_resultado)
+    return render(request,'resultado.html',{'resultados':lista_resultado,'columnas':nombre_columnas,'grafico':grafico})
 
 
 
